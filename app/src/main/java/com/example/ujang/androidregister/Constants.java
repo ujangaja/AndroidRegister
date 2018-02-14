042bfa24b5cc2197935b8d3aec9ff8672a76db3e
Ujang Arisandi <ujangaja@gmail.com>
2018-02-14 12:34:25 +0700

    membuat tampilan login

---
+++ app/src/main/java/com/example/ujang/androidregister/Constants.java
@@ -0,0 +1,13 @@
+package com.example.ujang.androidregister;
+
+/**
+ * Created by ujang on 2/14/2018.
+ */
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.243.1/Mysql-for-Android-Register/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+
+}
